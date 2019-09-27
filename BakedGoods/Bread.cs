a94d23eafe7c24f6700eb900f45b3f56ac8342ed
Devin Cooley <dcooley1350@gmail.com>
2019-09-27 13:50:46 -0700

    Fix logic in BuyTwoGetOne method

--- BakedGoods/Bread.cs
+++ BakedGoods/Bread.cs
@@ -21,7 +21,7 @@ namespace Pierre.BakedGoods
     {
       if (BOrderAmount >= 3)
       {
-      BOrderPrice = ((BOrderAmount * LoafPrice) - ((BOrderAmount / 3) * LoafPrice));
+      BOrderPrice = (((BOrderAmount-(BOrderAmount % 3))/3)*(2*LoafPrice) +((BOrderAmount % 3)*5))
       }
     }
   }
