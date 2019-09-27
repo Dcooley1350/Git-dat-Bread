7490a090e2ccb69634635e403ad37ec0c851fce6
Devin Cooley <dcooley1350@gmail.com>
2019-09-27 09:33:51 -0700

    Write pricing functiions for Pastries

--- BakedGoods/Pastry.cs
+++ BakedGoods/Pastry.cs
@@ -5,12 +5,28 @@ namespace Pierre.BakedGoods
     public string POrderFor { get; set; }
     public int  POrderAmount { get; set; }
     public double POrderPrice { get; set; }
+    public double PastryPrice { get; set; }
 
     public Pastry(string pOrderFor, int pOrderAmount)
     {
       POrderFor = pOrderFor;
       POrderAmount = pOrderAmount;
       POrderPrice = 0;
+      PastryPrice = 2;
+    }
+
+    public void  CalculatePOrderPrice()
+    {
+      POrderPrice = POrderAmount * PastryPrice;
+      ThreeForFive();
+    }
+
+    public void ThreeForFive()
+    {
+      if (POrderAmount >= 3)
+      {
+        POrderPrice = (((POrderAmount/3)*5)+((POrderAmount % 3)*2));
+      }
     }
   }
 }
